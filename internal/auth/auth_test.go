035f3b2a4191a65041d8857df077d5ee54f6b876
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2025-01-05 14:41:59 -0300

    test: auth GetAPIKey tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,46 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name    string
+		headers http.Header
+		want    string
+		wantErr error
+	}{
+		{
+			name:    "no auth header",
+			headers: http.Header{},
+			want:    "",
+			wantErr: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name:    "malformed auth header",
+			headers: http.Header{"Authorization": []string{"Bearer"}},
+			want:    "",
+			wantErr: ErrMalformedAuthHeader,
+		},
+		{
+			name:    "valid auth header",
+			headers: http.Header{"Authorization": []string{"ApiKey my-api-key"}},
+			want:    "my-api-key",
+			wantErr: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAPIKey(tt.headers)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if got != tt.want {
+				t.Errorf("GetAPIKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
